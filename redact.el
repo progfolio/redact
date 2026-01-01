0e5ede6f1b85719dcf9a2a71008c67f943e1ad10
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2026-01-01 12:10:18 -0500

    bump copyright year

--- redact.el
+++ redact.el
@@ -1,6 +1,6 @@
 ;;; redact.el --- Visually redact buffer text. -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023-2025  Nicholas Vollmer
+;; Copyright (C) 2023-2026  Nicholas Vollmer
 
 ;; Author: Nicholas Vollmer
 ;; Keywords: convenience
